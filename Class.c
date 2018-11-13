f76ba8e9b36753b2945d9932e13a191c4f424933
aalkhraissi <aalkhraissi@gmail.com>
2018-11-13 15:36:36 -0600

    test 2 GUI

--- Class.c
+++ Class.c
@@ -11,19 +11,31 @@
 
 #include <gtk/gtk.h>
 
-int main(int argc, char *argv[]) {
-    
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
     GtkWidget *window;
     
-    gtk_init(&argc, &argv);
-    
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show(window);
-    
-    g_signal_connect(window, "destroy",
-                     G_CALLBACK(gtk_main_quit), NULL);
-    
-    gtk_main();
-    
-    return 0;
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all (window);
 }
+
+int
+main (int    argc,
+      char **argv)
+{
+    GtkApplication *app;
+    int status;
+    
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+    
+    return status;
+}
+//        gcc `pkg-config --cflags gtk+-3.0` -o example-0 example-0.c `pkg-config --libs gtk+-3.0`
+
